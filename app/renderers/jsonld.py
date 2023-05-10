fded224df1c5c18bc927df026e09705e08ad66c2
Redmer Kronemeijer <12477216+redmer@users.noreply.github.com>
2023-05-10 10:33:00 +0000

    feat: improve JSON-LD rendering

--- app/renderers/jsonld.py
+++ app/renderers/jsonld.py
@@ -1,8 +1,7 @@
-from datetime import datetime
-
 from ..config import CANONICAL_HOST_BASE, FOTOWARE_FIELDNAME_UUID, FOTOWARE_HOST, HOST
 from ..fotoware.apitypes import Asset
 from ..slugify import slugify
+from mimetypes import guess_type
 
 
 def jsonldrender(asset: Asset) -> dict:
@@ -24,19 +23,23 @@ def jsonldrender(asset: Asset) -> dict:
     lname, ext = asset["filename"].split(".", maxsplit=1)
     filename = slugify(lname) + "." + ext
     subject = CANONICAL_HOST_BASE + identifier  # canonical
-    localurl = f"https://{HOST}/doc/{identifier}/{filename}"
-    remoteurl = FOTOWARE_HOST + asset["href"]
+    local_render = f"https://{HOST}/doc/{identifier}/{filename}"
+    fotoware_url = FOTOWARE_HOST + asset["href"]
+
+    mime = guess_type(filename)[0]
 
     return {
         "@id": subject,
         "@context": "https://schema.org/docs/jsonldcontext.json",
         "identifier": identifier,
-        "type": asset["doctype"].capitalize(),
-        "about": remoteurl,
-        "url": localurl,
-        "name": builtin_field("title"),
+        "dcterms:type": asset["doctype"],
+        "mainEntityOfPage": fotoware_url,
+        "url": local_render,
+        "name": lname,
+        "dcterms:title": builtin_field("title"),
         "description": builtin_field("description"),
         "keywords": builtin_field("tags"),
+        "encodingFormat": mime or "",
         "fileSize": asset["filesize"],
         "dateCreated": asset["created"],  # already ISO format
         "dateModified": asset["modified"],  # already ISO format
